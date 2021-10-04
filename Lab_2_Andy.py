9d65c7eeadf1d25c07bb68d43fbfc15682e33aa4
Andrew Klokov <555135@mail.ru>
2021-10-04 23:31:15 +0300

    Lab_2_Itib

--- Lab_2_Andy.py
+++ Lab_2_Andy.py
@@ -3,7 +3,7 @@ import numpy as np
 import random as rand
 
 data = np.array([[-4, 2, -3, 3, 12, 6]], float)  # Вариант 9 - c, d, a, b, N, A
-
+line = True # Лин.Функция = TRUE; Парабола = FALSE
 c = data[0][0]
 d = data[0][1]
 a = data[0][2]
@@ -15,35 +15,42 @@ def frange(a_ref, b_ref, jump):
     while a_ref < b_ref:
         yield a_ref
         a_ref += jump
-def createPoint_Yi(c_ref, d_ref, a_ref, b_ref, N_ref, A_ref):
+def createPoint_XYT(c_ref, d_ref, a_ref, b_ref, N_ref, A_ref):
     Xi = np.array([], float)
     Yi = np.array([], float)
+    Ti = np.array([], float)
     for i in frange(a_ref, b_ref, (b_ref - a_ref) / N_ref):
         err = A_ref * (rand.random() - 0.5)
         Xi = np.append(Xi, i)
-        Yi = np.append(Yi, c_ref * Xi[-1] + d_ref)
-    res = np.vstack([Xi, Yi])
+        Yi = np.append(Yi, c_ref * Xi[-1] + d_ref) if line == True else np.append(Yi, c_ref * Xi[-1] * Xi[-1] + d_ref)
+        Ti = np.append(Ti, Yi[-1] + err)
+    res = np.vstack([Xi, Yi, Ti])
+    res = np.around(res, decimals=1)
+    print("\nРаспределение points\n", res, "\n")
     return res
 
-print(createPoint_Yi(c, d, a, b, N))
-
-# def transInt_Float(mass_int, N):
-#     mass_float = []
-#     for i in range(N):
-#         mass_float.append(float(mass_int[i]))
-#     return mass_float
+def mnk(arr, N_ref):
+    for i in N_ref:
 
 
-# Xii = createPoint_Xi(data[0][2], data[0][3], data[0][4])
-# Yii = createPoint_Yi(data[0][0], data[0][1], data[0][4])
-# Xres = transInt_Float(Xii, data[0][4])
-# Yres = transInt_Float(Yii, data[0][4])
-# print(Xres)
-# print(Yres)
+def plotic(c_ref, d_ref, a_ref, b_ref, N_ref, A_ref):
+    arr = createPoint_XYT(c, d, a, b, N, A)
+    arrMNK = mnk(arr, N)
+    plt.title("Графики функции и разброса")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid()
+    plt.plot(arr[0], arr[1], '-b') # Исходная функция
+    plt.plot(arr[0], arr[2], 'or') # Значения (points)
+    plt.show()
 
-# plt.plot(Xii, Yii, '-ro')
 
-# one = []
-# for i in range(10):
-#     one.append(rand.random() - 0.5)
-# print(one)
+
+
+# ----------------------------------------
+# Запуск методов
+# ----------------------------------------
+plotic(c, d, a, b, N, A)
+
+
+# ----------------------------------------
