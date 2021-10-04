6680693a4b2a6446b7a109e4721e8a52ec53ede6
Andrew Klokov <555135@mail.ru>
2021-10-04 14:19:21 +0300

    add method "print_matrix" for formating print my matrix on console

--- Lab_2_Andy.py
+++ Lab_2_Andy.py
@@ -1,2 +1,20 @@
-print(5)
-print(77)
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.array([-4, 2, -3, 3, 12, 6])  # Вариант 9 - c, d, a, b, N, A
+
+arr_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+name_col = ["X", "Y", "Z"]
+name_tab = "Таблица XYZ"
+
+
+def print_matrix(arr, columns, name_table):
+    print("\n", name_table, sep="")
+    arr.insert(0, columns)
+    arr.insert(1, ["----"]*3)
+    for i in arr:
+        print(*[f"{x:>5}" for x in i])
+    print()
+
+print_matrix(arr_2d, name_col, name_tab)
+print_matrix(arr_2d, name_col, name_tab)
