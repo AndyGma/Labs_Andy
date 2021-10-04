6ed28a0cebd5dd43b3d771c4d3ddb415fdfd2791
Andrew Klokov <555135@mail.ru>
2021-10-04 22:30:16 +0300

    Lab_2_Itib

--- Lab_2_Andy.py
+++ Lab_2_Andy.py
@@ -4,45 +4,42 @@ import random as rand
 
 data = np.array([[-4, 2, -3, 3, 12, 6]], float)  # Вариант 9 - c, d, a, b, N, A
 
+c = data[0][0]
+d = data[0][1]
+a = data[0][2]
+b = data[0][3]
+N = data[0][4]
+A = data[0][5]
 
-def print_matrix(arr, columns, name_table):
-    print("\n", name_table, sep="")
-    arr.insert(0, columns)
-    arr.insert(1, ["----"]*len(columns))
-    for i in arr:
-        print(*[f"{x:>5}" for x in i])
-    print()
-    arr.pop(1)
-    arr.pop(0)
-def frange(a, b, jump):
-    while a < b:
-        yield a
-        a += jump
-def createPoint_Xi(a, b, N):
-    ret = []
-    for i in frange(a, b, (b - a) / N):
-        ret.append(i)
-    return ret
-def createPoint_Yi(c, d, N):
-    Xi = createPoint_Xi(data[0][2], data[0][3], data[0][4])
-    Yi = []
-    for i in range(N):
-        Yi.append(c * Xi[i] + d)
-    return Yi
-def transInt_Float(mass_int, N):
-    mass_float = []
-    for i in range(N):
-        mass_float.append(float(mass_int[i]))
-    return mass_float
+def frange(a_ref, b_ref, jump):
+    while a_ref < b_ref:
+        yield a_ref
+        a_ref += jump
+def createPoint_Yi(c_ref, d_ref, a_ref, b_ref, N_ref, A_ref):
+    Xi = np.array([], float)
+    Yi = np.array([], float)
+    for i in frange(a_ref, b_ref, (b_ref - a_ref) / N_ref):
+        err = A_ref * (rand.random() - 0.5)
+        Xi = np.append(Xi, i)
+        Yi = np.append(Yi, c_ref * Xi[-1] + d_ref)
+    res = np.vstack([Xi, Yi])
+    return res
+
+print(createPoint_Yi(c, d, a, b, N))
+
+# def transInt_Float(mass_int, N):
+#     mass_float = []
+#     for i in range(N):
+#         mass_float.append(float(mass_int[i]))
+#     return mass_float
 
 
-print_matrix(data, ["c", "d", "a", "b", "N", "A"], "Исходные данные")
-Xii = createPoint_Xi(data[0][2], data[0][3], data[0][4])
-Yii = createPoint_Yi(data[0][0], data[0][1], data[0][4])
-Xres = transInt_Float(Xii, data[0][4])
-Yres = transInt_Float(Yii, data[0][4])
-print(Xres)
-print(Yres)
+# Xii = createPoint_Xi(data[0][2], data[0][3], data[0][4])
+# Yii = createPoint_Yi(data[0][0], data[0][1], data[0][4])
+# Xres = transInt_Float(Xii, data[0][4])
+# Yres = transInt_Float(Yii, data[0][4])
+# print(Xres)
+# print(Yres)
 
 # plt.plot(Xii, Yii, '-ro')
 
--- testing.py
+++ testing.py
@@ -1,2 +1,10 @@
-list1 = [1,2,3,4,5]
-list2 = [6,7,8,9,0]
+import numpy as np
+
+def createPoint_Yi():
+    Xi = np.array([-3, -2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5], float)
+    Yi = np.array([], float)
+    for i in range(12):
+        res = np.append(Yi, [-4 * Xi + 2])
+    return res
+
+print(createPoint_Yi())
