0a00f1c6d2520c992367401f533f0f08e7411556
Andrew Klokov <555135@mail.ru>
2021-10-06 12:43:01 +0300

    Lab_2_Itib Complete

--- Lab_2_Anya.py
+++ Lab_2_Anya.py
@@ -4,9 +4,9 @@ import random as rand
 
 rand.seed(1)
 
-data = np.array([[-4, 2, -3, 3, 12, 6]], float)  # Вариант 9 - c, d, a, b, N, A
-# data = np.array([[-4, 2, -1, 2, 12, 5]], float)  # Вариант 2 - c, d, a, b, N, A
-line = True  # Лин.Функция = TRUE; Парабола = FALSE
+# data = np.array([[-4, 2, -3, 3, 12, 6]], float)  # Вариант 9 - c, d, a, b, N, A
+data = np.array([[-4, 2, -1, 2, 12, 5]], float)  # Вариант 2 - c, d, a, b, N, A
+line = False  # Лин.Функция = TRUE; Парабола = FALSE
 c = data[0][0]
 d = data[0][1]
 a = data[0][2]
